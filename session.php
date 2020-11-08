31358487509b4b955b6b31e3c6d2b9c8f2997372
Muaz Dervent <dervent.muaz@std.izu.edu.tr>
2020-11-09 00:29:52 +0300

    session.php has been changed so that the kubernetes jitsi ip and port to be connected to are found from the db.

--- session.php
+++ session.php
@@ -21,15 +21,20 @@
  * if you like, and it can span multiple lines.
  *
  * @package    mod_jitsi
- * @copyright  2019 Sergio Comerón <sergiocomeron@icloud.com>
+ * @copyright  2020 Muaz Dervent <muazdervent@gmail.com>
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
+global $USER;
+
 
 require_once(dirname(dirname(dirname(__FILE__))).'/config.php');
 require_once(dirname(dirname(dirname(__FILE__))).'/lib/moodlelib.php');
 require_once(dirname(__FILE__).'/lib.php');
 $PAGE->set_url($CFG->wwwroot.'/mod/jitsi/session.php');
 
+
+
+$themeconfig = theme_config::load($CFG->theme);
 $courseid = required_param('courseid', PARAM_INT);
 $cmid = required_param('cmid', PARAM_INT);
 $nombre = required_param('nom', PARAM_TEXT);
@@ -43,6 +48,12 @@ $PAGE->set_title($session);
 $PAGE->set_heading($session);
 echo $OUTPUT->header();
 
+$db_ip = "x.x.x.x";           //replace  
+$db_port = "3306";            //these
+$db_username = "user";        //parameters
+$db_password = "password";    //with
+$db_db_name = "jitsi_moodle"; //yours
+
 if ($teacher == 1) {
       $teacher = true;
 } else {
@@ -54,6 +65,17 @@ $context = context_module::instance($cmid);
 if (!has_capability('mod/jitsi:view', $context)) {
     notice(get_string('noviewpermission', 'jitsi'));
 }
+
+//son eklenen
+
+$roles = get_user_roles($context, $USER->id);
+
+$rolestr[] = null;
+foreach ($roles as $role) {
+    $rolestr[] = $role->shortname;
+} //son eklenene
+
+
 
 $header = json_encode([
   "kid" => "jitsi/custom_key_name",
@@ -86,11 +108,50 @@ $secret = $CFG->jitsi_secret;
 $signature = hash_hmac('sha256', $base64urlheader . "." . $base64urlpayload, $secret, true);
 $base64urlsignature = str_replace(['+', '/', '='], ['-', '_', ''], base64_encode($signature));
 
+
+
+//----------- i added this for re-orchestrator
+$link = new mysqli($db_ip, $db_username, $db_password, $db_db_name, $db_port);
+$sql = "SELECT jitsi_web_port FROM jibri_sessions where session_id='".$sessionnorm."'";
+if($result = mysqli_query($link, $sql)){
+    if(mysqli_num_rows($result) > 0){
+
+        while($row = mysqli_fetch_array($result)){
+            $jitsi_web_port = $row['jitsi_web_port'];
+        }
+        // Free result set
+        mysqli_free_result($result);
+
+    }else if((in_array('editingteacher', $rolestr) == 1) || (in_array('manager', $rolestr) == 1) || (in_array('coursecreator', $rolestr) == 1)){
+        //The URL with parameters / query string.
+        //     this ip and port can be diffrent web server and this query should create a jitsi deployment on your kubernetes server.
+        $url = "http://your_node_js_ip_and_port/create_jitsi_deployment?param1=create&param2=".$sessionnorm;
+        //Once again, we use file_get_contents to GET the URL in question.
+        $contents = file_get_contents($url);
+        if($contents !== false){
+            //catch created deployment port.
+            //Print out the contents.
+            $jitsi_web_port = $contents;
+        }
+
+    }else {
+        echo "<script> window.location.href='".$CFG->wwwroot."/course/view.php?id=".$courseid."'</script>;\n";
+    }
+} else{
+    echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
+}
+ 
+// Close connection
+mysqli_close($link);
+//------------added end*
+
+
+
 $jwt = $base64urlheader . "." . $base64urlpayload . "." . $base64urlsignature;
-echo "<script src=\"https://".$CFG->jitsi_domain."/external_api.js\"></script>\n";
+echo "<script src=\"https://".$CFG->jitsi_domain.":".$jitsi_web_port."/external_api.js\"></script>\n";
 
 echo "<script>\n";
-echo "var domain = \"".$CFG->jitsi_domain."\";\n";
+echo "var domain = \"".$CFG->jitsi_domain.":".$jitsi_web_port."\";\n";
 echo "var options = {\n";
 echo "configOverwrite: {\n";
 echo "channelLastN: ".$CFG->jitsi_channellastcam.",\n";
@@ -164,21 +225,11 @@ echo "api.executeCommand('avatarUrl', '".$avatar."');\n";
 if ($CFG->jitsi_finishandreturn == 1) {
     echo "api.on('readyToClose', () => {\n";
     echo      "api.dispose();\n";
-    echo      "location.href=\"".$CFG->wwwroot."/course/view.php?id=".$courseid."\";";
+  //echo      "location.href=\"".$CFG->wwwroot."/course/view.php?id=".$courseid."\";";
+    echo      "location.href=\"".$CFG->wwwroot."/mod/jitsi/view.php?id=".$cmid."\";";
     echo  "});\n";
 }
 
-if ($CFG->jitsi_password != null) {
-    echo "api.addEventListener('participantRoleChanged', function(event) {";
-    echo "    if (event.role === \"moderator\") {";
-    echo "        api.executeCommand('password', '".$CFG->jitsi_password."');";
-    echo "    }";
-    echo "});";
-    echo "api.on('passwordRequired', function ()";
-    echo "{";
-    echo "    api.executeCommand('password', '".$CFG->jitsi_password."');";
-    echo "});";
-}
 
 echo "</script>\n";
 
